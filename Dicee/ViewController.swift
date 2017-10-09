66394dc7b75104a6347529897ed95529d785abeb
Sain-R Edwards Jr <SKE@sain-rs-imac-5.local>
2017-10-09 13:06:01 -0700

    1. Created functions and code for app to operate as it should. 2. Added constraints to the UI elements on the Storyboard for auto layout consistency.

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,12 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    @IBOutlet weak var rollButtonImageView: UIButton!
+    
+    let diceArray: [String] = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateDiceGame()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,6 +34,22 @@ class ViewController: UIViewController {
 
     @IBAction func rollButtonTapped(_ sender: UIButton) {
         
+        updateDiceGame()
+        
+    }
+    
+    func updateDiceGame() {
+        
+        rollButtonImageView.layer.cornerRadius = rollButtonImageView.bounds.width / 3.5
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateDiceGame()
     }
     
 }
